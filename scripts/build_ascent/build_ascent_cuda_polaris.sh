2bda4019abd2e54dab3a03ccade5cfa967edd0b6
Qi Wu <qwu@anl.gov>
2024-04-30 19:49:50 +0000

    update build script

--- scripts/build_ascent/build_ascent_cuda_polaris.sh
+++ scripts/build_ascent/build_ascent_cuda_polaris.sh
@@ -7,10 +7,10 @@ export http_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
 export https_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
 export ftp_proxy="http://proxy-01.pub.alcf.anl.gov:3128"
 
-source /home/qiwu/projects/diva_superbuild/setup-env.sh
+source /home/qiwu/env-polaris.sh
 
 export CC=$(which cc)
 export CXX=$(which CC)
 export FTN=$(which ftn)
 
-env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF build_ascent=false ./build_ascent_cuda.sh
+env enable_mpi=ON enable_fortran=ON raja_enable_vectorization=OFF build_ascent=false CUDA_ARCH=80 ./build_ascent_cuda.sh
